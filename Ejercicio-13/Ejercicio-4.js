58f26d5ce691020b8efe09d9bf4914672a691112
joel-jalixto <joeljalixto20@gmail.com>
2022-11-13 23:19:01 -0500

    ejercicio-13

---
+++ Ejercicio-13/Ejercicio-4.js
@@ -0,0 +1,13 @@
+
+let nombre = "Joel"
+let apellido = `Jalixto`
+let estudiante = "Estudiante"+ nombre +" "+ apellido
+let estudianteMayus = estudiante.toUpperCase()
+let estudianteMinus = estudiante.toLowerCase()
+let estudianteLetras = estudiante.length
+let nombrePrimLetra = nombre.charAt(0,1)
+let apellidoUltLetra = apellido.charAt(6)
+let estudianteEspacio = estudiante.trim()
+let boleean = estudiante
+
+console.log(boleean.includes("Joel"));

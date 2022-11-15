bf4d755bf79ad484c943584e7a382368ee824bd9
joel-jalixto <joeljalixto20@gmail.com>
2022-11-15 11:03:35 -0500

    Ejercicio-14

---
+++ Ejercicio-14/intro.js
@@ -0,0 +1,13 @@
+
+const buton = document.getElementById("btn")
+buton.addEventListener("click", () => {
+    alert("Click en el botón");
+})
+
+
+const boton = document.querySelector("#btn")
+boton.addEventListener("click", () => {
+    console.log("Hola estoy utilizando jQuery");
+})
+
+
